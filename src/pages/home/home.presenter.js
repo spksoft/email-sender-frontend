386c974af9dbec4243471a4b35615d119c55429f
spksoft NA <spkrsk.37@gmail.com>
2019-01-07 18:44:17 +0700

    add form

--- src/pages/home/home.presenter.js
+++ src/pages/home/home.presenter.js
@@ -1,9 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import { Card, Input } from 'antd';
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Body = styled.div`
+  display: flex;
+  width: 800px;
+  padding: 20px;
+  flex-direction: column;
+`
+
+const RowContainer = styled.div`
+  padding: 3px;
+`
 
 const Presenter = () => (
-  <div>
-    <p>Hello, Home</p>
-  </div>
+  <PageContainer>
+    <Body>
+      <Card
+        title="Email Sender"
+      >
+        <RowContainer><Input addonBefore="From: " /></RowContainer>
+        <RowContainer><Input addonBefore="To: " /></RowContainer>
+        <RowContainer><Input addonBefore="Subject" /></RowContainer>
+        <RowContainer><h4>Content: </h4></RowContainer>
+        <RowContainer><Input.TextArea rows={4}></Input.TextArea></RowContainer>
+      </Card>
+    </Body>
+  </PageContainer>
 )
 
 export default Presenter
